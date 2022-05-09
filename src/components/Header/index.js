b8e1ba531694c0a28b9b9e714df4ab1c19f23ebb
彭格列 <1801688332@qq.com>
2022-05-09 20:10:10 +0800

    feat: header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React, { memo } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Input } from 'antd';
+import { SearchOutlined } from '@ant-design/icons'
 
 import { headerLinks } from '@/common/local-data';
 
@@ -10,7 +12,10 @@ export default memo(function PglHeader() {
   const showSelectItem = (item, index) => {
     if (index < 3) {
       return (
-        <NavLink to={item.link}>{item.title}</NavLink>
+        <NavLink to={item.link} exact>
+          {item.title}
+          <i className="sprite_01 icon"></i>
+        </NavLink>
       )
     } else {
       return <a href={item.link}>{item.title}</a>
@@ -34,7 +39,11 @@ export default memo(function PglHeader() {
             }
           </div>
         </HeaderLeft>
-        <HeaderRight>right</HeaderRight>
+        <HeaderRight>
+          <Input className="search" placeholder="音乐/视频/电台/用户" prefix={<SearchOutlined />}/>
+          <div className="center">创作者中心</div>
+          <div>登录</div>
+        </HeaderRight>
       </div>
       <div className="divider"></div>
     </HeaderWrapper>
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -11,7 +11,6 @@ export const HeaderWrapper = styled.div`
 
   .content {
     height: 70px;
-    background-color: aquamarine;
 
     display: flex;
     justify-content: space-between;
@@ -76,4 +75,33 @@ export const HeaderLeft = styled.div`
   }
 `;
 
-export const HeaderRight = styled.div``;
+export const HeaderRight = styled.div`
+display: flex;
+align-items: center;
+color: #ccc;
+font-size: 12px;
+
+
+.search {
+  width: 158px;
+  height: 32px;
+  border-radius: 16px;
+
+  input {
+    &::placeholder {
+      font-size: 12px;
+    }
+  }
+}
+
+.center {
+  width: 90px;
+  height: 32px;
+  line-height: 32px;
+  text-align: center;
+  border: 1px solid #666;
+  border-radius: 16px;
+  margin: 0 16px;
+  background-color: transparent;
+}
+`;
