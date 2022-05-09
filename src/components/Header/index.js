b80448f82cd11e766e541518af2be1bec3ef2058
彭格列 <1801688332@qq.com>
2022-05-09 19:56:03 +0800

    feat: 配置头部样式

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,41 @@
 import React, { memo } from 'react';
-import { HeaderWrapper } from './style';
+import { NavLink } from 'react-router-dom';
+
+import { headerLinks } from '@/common/local-data';
+
+import { HeaderWrapper, HeaderLeft, HeaderRight } from './style';
 
 export default memo(function PglHeader() {
+
+  const showSelectItem = (item, index) => {
+    if (index < 3) {
+      return (
+        <NavLink to={item.link}>{item.title}</NavLink>
+      )
+    } else {
+      return <a href={item.link}>{item.title}</a>
+    }
+  }
+
   return (
     <HeaderWrapper>
-      <div className="content wrap-v1"></div>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <a href="#/" className="logo sprite_01"> </a>
+          <div className="select-list">
+            {
+              headerLinks.map((item, index) => {
+                return (
+                  <div key={index} className="select-item">
+                    {showSelectItem(item, index)}
+                  </div>
+                )
+              })
+            }
+          </div>
+        </HeaderLeft>
+        <HeaderRight>right</HeaderRight>
+      </div>
       <div className="divider"></div>
     </HeaderWrapper>
   )
