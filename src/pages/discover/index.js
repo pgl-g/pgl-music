375c118dfec6d98339d823cc0bcc36abb2c81b31
彭格列 <1801688332@qq.com>
2022-05-14 22:55:05 +0800

    feat: 子菜单栏创建

--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,9 +1,30 @@
 import React, { memo } from 'react';
 
+import { NavLink } from 'react-router-dom';
+
+import { dicoverMenu } from "@/common/local-data";
+
+import {
+  DiscoverWrapper,
+  TopMenu
+} from './style';
+
 export default memo(function PglDiscover() {
   return (
-    <div>
-      PglDiscover
-    </div>
+    <DiscoverWrapper>
+      <div className="top">
+      <TopMenu className="wrap-v1">
+        {
+          dicoverMenu.map((item, index) => {
+            return (
+              <div className="item" key={item.title}>
+                <NavLink to={item.link}>{item.title}</NavLink>
+              </div>
+            )
+          })
+        }
+      </TopMenu>
+      </div>
+    </DiscoverWrapper>
   )
 })
